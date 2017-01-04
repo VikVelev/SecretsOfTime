88b7fdc0a752a5a6e6a7a280ff48b89540e05437
VikVelev <viktorvelev8@gmail.com>
2017-01-04 23:13:31 +0200

    ?
    ?

--- Secrets of Time/Assets/Scripts/Teleport.cs
+++ Secrets of Time/Assets/Scripts/Teleport.cs
@@ -4,55 +4,81 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour {
 
-    public GameObject Player;
-    public GameObject TimeMachine;
-    public GameObject[] Rooms;
-    public bool[] clicked;
-    int Choice = 0;   
+    //Initializing variables
+    public GameObject Player; 
+    public GameObject TimeMachine; //Not yet used
+    public GameObject[] Rooms; //All the rooms  
+    public bool[] clicked; //Array that checks if you've clicked the room on the cube
+    new Animation animation; //Animation component 
+    int Choice = 0; //User Choice 0 means nothing not even the start position
     
     void Start() {
 
-        
+        animation = GetComponent<Animation>();
+    }
+
+    Vector3 GetPosition(GameObject _Object)
+    {
+        return _Object.transform.position;
+    }
+
+    void Choose()
+    {
+        if (Choice == 0)
+        {
+            //Otherwise searches for nonexistent Array index -1 
+        }
+        else if (clicked[Choice - 1]) //Checks if you have clicked the current choice, if you have , well you haven't.
+        {
+            clicked[Choice - 1] = false;
+        }
+
+        Choice++;
+        animation.Play("choice");
+
+        if (Choice == 6) //Loops
+        {
+            Choice = 1;
+        }
+        Debug.Log("Room " + Choice);
+    }
+
+    void Teleportation()
+    {
+        if (Choice == 0)
+        { 
+            //Otherwise searches for nonexistent Array index -1
+        }
+        else
+        {
+            if (!clicked[Choice - 1]) //If it isn't clicked, teleport
+            {
+                Player.transform.position = GetPosition(Rooms[Choice - 1]);
+                clicked[Choice - 1] = true;
+                Debug.Log("You are now in Room " + Choice);
+                animation.Play("teleport");
+            }
+            else if (clicked[Choice - 1]) //If it is clicked, well no teleport for you.
+            {
+                Debug.Log("You are still in the same room");
+            }
+        }
     }
 
     void Update() {
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Choice == 0)
-            { }
-            else
-            {
-                if (!clicked[Choice - 1])
-                {
-                    Player.transform.position = Rooms[Choice - 1].transform.position;
-                    clicked[Choice - 1] = true;
-                    Debug.Log("You are now in Room " + Choice);
-                }
-                else if (clicked[Choice - 1])
-                {
-                    Debug.Log("You are still in the same room");
-                }
-            }
+            Teleportation();
         } 
         else if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            if(Choice == 0)
-            { }
-            else if (clicked[Choice - 1])
-            {
-                clicked[Choice - 1] = false;
-            }
-
-            Choice++;
-                      
-            if (Choice == 6)
-            { 
-                Choice = 1;
-            }
-
-            Debug.Log("Room " + Choice);
-
+            Choose();
         }
+        if (animation.isPlaying == false)
+        {
+            animation.Play("idle");
+        }
+
     }
 }
-
