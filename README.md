2eada96589d389d0118682c124a9a9fc3ce05611
VikVelev <viktorvelev8@gmail.com>
2017-02-16 23:33:01 +0200

    Start Menu

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# secrets-of-time
+Secrets of Time!
+
 An educational game about traveling through time and experiencing the technology evolution throughout the years.
+
+Using: Unity, C#, Cinema4D, 3DSmax, Photoshop
---
+++ Secrets-Of-Time-Official/Assets/Scripts/scene_start.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_start : MonoBehaviour {
+    public GameObject[] todisable;
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+        for (int i = 0; i < todisable.Length; i++)
+        {
+            todisable[i].gameObject.SetActive(false);
+        }
+        this.gameObject.SetActive(false);
+
+    }
+}
