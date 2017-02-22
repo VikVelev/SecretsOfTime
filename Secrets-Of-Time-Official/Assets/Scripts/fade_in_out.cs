615754e5c46c7b5ef6a48337802d7a31597bb5cb
VikVelev <viktorvelev8@gmail.com>
2017-02-22 22:55:27 +0200

    a lot of functionality added + pause menu

---
+++ Secrets-Of-Time-Official/Assets/Scripts/fade_in_out.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class fade_in_out : MonoBehaviour {
+
+    public Text[] txt = new Text[1];
+    public Image[] img = new Image[1];
+    public Canvas root;
+    public float fade_time = 0.1f;
+
+    void Start () {
+ 
+    }
+
+    void Update()
+    {
+            if (!root.GetComponent<Canvas>().enabled)
+            {
+                if (txt == null)
+                {
+                    for (int i = 0; i < img.Length; i++)
+                    {
+                        img[i].CrossFadeAlpha(0, fade_time, false);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < txt.Length; i++)
+                    {
+                        try
+                        {
+                            txt[i].CrossFadeAlpha(0, fade_time, false);
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+                }
+            if (txt != null && img != null)
+            {
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    try
+                    {
+                        txt[i].CrossFadeAlpha(0, fade_time, false);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                for (int i = 0; i < img.Length; i++)
+                {
+                    img[i].CrossFadeAlpha(0, fade_time, false);
+                }
+            }
+        }
+            else
+            {
+                if (txt == null && img != null)
+                {
+                    for (int i = 0; i < img.Length; i++)
+                    {
+                        img[i].CrossFadeAlpha(1f, fade_time, false);
+                    }
+                }
+                else
+                {
+                }
+
+                if (txt != null && img != null)
+                {
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    try
+                    {
+                        txt[i].CrossFadeAlpha(1, fade_time, false);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                for (int i = 0; i < img.Length; i++)
+                {
+                    img[i].CrossFadeAlpha(1f, fade_time, false);
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Secrets-Of-Time-Official/Assets/Scripts/pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour {
+    public Camera MainCamera;
+    public GameObject current_UI;
+    public GameObject pause_UI;
+    public GameObject player;
+    bool paused;
+
+    public void pausetoggle()
+    {
+        if (!paused)
+        {
+            player.GetComponent<crosshair>().enabled = false;
+            current_UI.GetComponent<Canvas>().enabled = false;            
+            pause_UI.GetComponent<Canvas>().enabled = true;           
+            paused = true;
+            Time.timeScale = 0;
+            
+        }
+        else
+        {            
+            Time.timeScale = 1;
+            current_UI.GetComponent<Canvas>().enabled = true;
+            pause_UI.GetComponent<Canvas>().enabled = false;
+            player.GetComponent<crosshair>().enabled = true;
+
+            paused = false;
+
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausetoggle();
+        }
+    }
+}
